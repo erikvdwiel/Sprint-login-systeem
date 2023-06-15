3398c19f8caa88f652bb2ae986e10e9d05b5972f
erikvdwiel <9019165@student.zadkine.nl>
2023-06-15 11:50:55 +0200

    registreren

--- Sprint 1 login/registreren.php
+++ Sprint 1 login/registreren.php
@@ -3,16 +3,16 @@
 
 <body>
     </form action="login_validate.php" method="POST">
-<h1>Inloggen</h1><br>
+<h1>Registreren</h1><br>
         <label for="gebruikersnaam">Gebruikersnaam:</label>
         <input type="text" name="gebruikersnaam" id="gebruikersnaam" required>
         <br>
         <label for="password">Wachtwoord:</label>
         <input type="password" name="password" id="password" required>
         <br>
-        <input type="submit" value="Log in" name="submit"><br>
+        <input type="submit" value="Registreren" name="submit"><br>
         </form> <br>
-        <a href="registreren.php">Registreren</a>
+        <a href="index.php">Terug</a>
 
 </body>
 </html>
